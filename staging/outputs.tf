502f0aa25f6f22e09d71f823e676d344134ebc77
izabelacarrion <izabelacarrioncastro@gmail.com>
2026-03-03 21:33:25 -0300

    Adiciona output do SG WEB

--- staging/outputs.tf
+++ staging/outputs.tf
@@ -12,3 +12,8 @@ output "ip_privado_db" {
   value       = module.ec2-staging.instance_ip_db_addr
   description = "IP privado da instancia DB"
 }
+
+output "security_group_id_web" {
+  value       = module.ec2-staging.sg_id # Puxa o ID do SG de dentro do módulo
+  description = "ID do Security Group que o GitHub Actions vai manipular"
+}
